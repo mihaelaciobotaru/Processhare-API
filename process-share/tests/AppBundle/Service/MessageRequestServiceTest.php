231255b08878f287133bae7fbe3a8a8a4c8c50db
Constantin Dogaru <constantin.dogaru@emag.ro>
2017-09-10 04:14:12 +0300

    added messagerequest test

---
+++ process-share/tests/AppBundle/Service/MessageRequestServiceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\AppBundle\Service;
+
+use AppBundle\Service\MessageRequestService;
+use PHPUnit\Framework\TestCase;
+
+class MessageRequestServiceTest extends TestCase
+{
+    public function testComputeMessageCacheKey()
+    {
+        $message = array();
+        $current = new \DateTime();
+        $message['timestamp'] = $current->format('Y-m-d H:i:s');
+        $message['data'] = array(1, 2, 3, 4, 5);
+        $expected = sha1($message['timestamp'] . json_encode($message['data']));
+        
+        $service = new MessageRequestService();
+        $this->assertEquals($expected, $service->computeMessageCacheKey($message));
+    }
+}
